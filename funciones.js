98d8766caaa2aac33748c330a91a3e1f2386285d
OrionAlzategit config -- global user.name RQRgit config --global user.name OrionAlzate <orion_alzate@hotmail.com>
2023-11-20 20:46:55 -0500

    se arregla tiempo

--- funciones.js
+++ funciones.js
@@ -116,20 +116,21 @@ let imgN2 = [
     url: "imagenes/tori.jpg",
   },
 ];
-let tablero = d.querySelector(".tablero");
-let imagenNombre = [];
-let imagenID = [];
-let aciertos = 0;
-let intentos = 0;
-let tiempo = 60;
-let nivel = 1;
-let mostrarNiv = d.querySelector(".nivel");
-let mostrarInt = d.querySelector(".intentos");
-let mostrarAci = d.querySelector(".aciertos");
-let mostrarTime = d.querySelector(".tiempo");
-let btnStart = d.querySelector(".btn-iniciar");
-let tiempoTranscurrido;
+let tablero = d.querySelector(".tablero")
+let imagenNombre = []
+let imagenID = []
+let aciertos = 0
+let intentos = 0
+let tiempo = 60
+let nivel = 1
+let mostrarNiv = d.querySelector(".nivel")
+let mostrarInt = d.querySelector(".intentos")
+let mostrarAci = d.querySelector(".aciertos")
+let mostrarTime = d.querySelector(".tiempo")
+let btnStart = d.querySelector(".btn-iniciar")
+let tiempoTranscurrido
 let imagenNivel
+let estoyJugando = false
 
 //agregar evento al boton para iniciar el juego
 function limpiarTablero() {
@@ -138,14 +139,12 @@ function limpiarTablero() {
 btnStart.addEventListener("click", function () {
   alert("¡Haz iniciado el juego!");
   limpiarTablero();
-  //controlar el tiempo
-  //se ejecuta en determinado timpo infinitamente
-  tiempoTranscurrido = setInterval(() => {
-    tiempo--;
-    if (nivel === 2) {
+  if(estoyJugando == false && nivel == 1){
+    estoyJugando = true
+    //controlar el tiempo
+    tiempoTranscurrido = setInterval(() => {
       tiempo--;
-    }
-    mostrarTime.textContent = tiempo;
+      mostrarTime.textContent = tiempo;
     if (tiempo == 10) {
       alert("¡Rapido el tiempo se esta agotando¡🤯!Tienes 10s¡");
       mostrarTime.style.color = "red";
@@ -155,8 +154,8 @@ btnStart.addEventListener("click", function () {
       clearInterval(tiempoTranscurrido);
       location.reload();
     }
-  }, 1000);
-
+  }, 1000); 
+}
   //agregar las imagenes al tablero
   agregarImagenes();
   //mostrat nivel
@@ -165,94 +164,96 @@ btnStart.addEventListener("click", function () {
 
 //funcion para agregar img al tablero
 function agregarImagenes() {
-  if(nivel == 1){
-    imagenNivel = imgN1
+  if (nivel == 1) {
+    imagenNivel = imgN1;
     imagenNivel.forEach((i) => {
-      let div = d.createElement("div")
-      div.className = "col-3"
-      let img = d.createElement("img")
-      img.className = "img-fluid alto-img"
-      img.id = imagenNivel.indexOf(i)
-      img.src = "imagenes/ocultar.jpg"
-      img.addEventListener("click", mostrarImg)
-      div.appendChild(img)
-      tablero.appendChild(div)
+      let div = d.createElement("div");
+      div.className = "col-3";
+      let img = d.createElement("img");
+      img.className = "img-fluid alto-img";
+      img.id = imagenNivel.indexOf(i);
+      img.src = "imagenes/ocultar.jpg";
+      img.addEventListener("click", mostrarImg);
+      div.appendChild(img);
+      tablero.appendChild(div);
     });
-  } else if (nivel == 2){
-    imagenNivel = imgN2
+  } else if (nivel == 2) {
+    imagenNivel = imgN2;
     imagenNivel.forEach((i) => {
-      let div = d.createElement("div")
-      div.className = "col-3"
-      let img = d.createElement("img")
-      img.className = "img-fluid alto-img"
-      img.id = imagenNivel.indexOf(i)
-      img.src = "imagenes/ocultar.jpg"
-      img.addEventListener("click", mostrarImg)
-      div.appendChild(img)
-      tablero.appendChild(div)
+      let div = d.createElement("div");
+      div.className = "col-3";
+      let img = d.createElement("img");
+      img.className = "img-fluid alto-img";
+      img.id = imagenNivel.indexOf(i);
+      img.src = "imagenes/ocultar.jpg";
+      img.addEventListener("click", mostrarImg);
+      div.appendChild(img);
+      tablero.appendChild(div);
     });
   }
 }
 
 //funcion para mostrar las imagenes ocultas
 function mostrarImg() {
-  let imgID = parseInt(this.getAttribute("id"))
+  let imgID = parseInt(this.getAttribute("id"));
   // Verificar si la imagen ya ha sido acertada
   if (this.src.includes("correcto.png")) {
-    alert("Ya seleccionaste esta imagen correctamente. Elige otra.")
-    return
+    alert("Ya seleccionaste esta imagen correctamente. Elige otra.");
+    return;
   }
 
   // Verificar si se ha elegido la misma imagen
   if (imagenID.includes(imgID)) {
-    alert("Debes escoger una imagen diferente.")
+    alert("Debes escoger una imagen diferente.");
     intentos++;
-    mostrarInt.textContent = intentos
-    return
+    mostrarInt.textContent = intentos;
+    return;
   }
 
-  this.src = imagenNivel[imgID].url
-  imagenNombre.push(imagenNivel[imgID].nombre)
-  imagenID.push(imgID)
+  this.src = imagenNivel[imgID].url;
+  imagenNombre.push(imagenNivel[imgID].nombre);
+  imagenID.push(imgID);
 
   if (imagenNombre.length == 2) {
-    setTimeout(compararImg, 200)
+    setTimeout(compararImg, 200);
   }
 }
 
 //funcion para comparar imagenes
 function compararImg() {
-  let imgTablero = d.querySelectorAll(".tablero  div  img")
+  let imgTablero = d.querySelectorAll(".tablero  div  img");
   if (imagenNombre[0] == imagenNombre[1]) {
-    alert("¡Felicitaciones adivinaste una imagen!")
-    imgTablero[imagenID[0]].src = "imagenes/correcto.png"
-    imgTablero[imagenID[1]].src = "imagenes/correcto.png"
-    aciertos++
-    mostrarAci.textContent = aciertos
+    alert("¡Felicitaciones adivinaste una imagen!");
+    imgTablero[imagenID[0]].src = "imagenes/correcto.png";
+    imgTablero[imagenID[1]].src = "imagenes/correcto.png";
+    aciertos++;
+    mostrarAci.textContent = aciertos;
   } else {
     alert("Fallaste las imagenes son diferentes");
-    imgTablero[imagenID[0]].src = "imagenes/ocultar.jpg"
-    imgTablero[imagenID[1]].src = "imagenes/ocultar.jpg"
-    intentos++
-    mostrarInt.textContent = intentos
+    imgTablero[imagenID[0]].src = "imagenes/ocultar.jpg";
+    imgTablero[imagenID[1]].src = "imagenes/ocultar.jpg";
+    intentos++;
+    mostrarInt.textContent = intentos;
   }
 
-  imagenNombre = []
-  imagenID = []
+  imagenNombre = [];
+  imagenID = [];
 
   //comprobar si adivinaron todas la img
   if (nivel == 1 && aciertos == 6) {
-    alert("🎉🎖️¡Felicitaciones pasaste al siguiente nivel!🎖️🎉")
-    nivel++
-    mostrarNiv.textContent = nivel
-    intentos = 0
-    mostrarInt.textContent = intentos
-    aciertos = 0
-    mostrarAci.textContent = aciertos
-    clearInterval(tiempoTranscurrido)
-    tiempo = 50
-    mostrarTime.textContent = tiempo
+    alert("🎉🎖️¡Felicitaciones pasaste al siguiente nivel!🎖️🎉");
+    nivel++;
+    mostrarNiv.textContent = nivel;
+    intentos = 0;
+    mostrarInt.textContent = intentos;
+    aciertos = 0;
+    mostrarAci.textContent = aciertos;
+    clearInterval(tiempoTranscurrido);
+    tiempo = 50;
+    mostrarTime.textContent = tiempo;
   } else if (nivel == 2 && aciertos == 8) {
-    alert("🎉🎖️¡Felicitaciones pasaste al siguiente nivel!🎖️🎉")
+    alert("🎉🎖️¡Felicitaciones pasaste al siguiente nivel!🎖️🎉");
+    clearInterval(tiempoTranscurrido);
+    alert("¡Ganaste el juego! Tiempo detenido.");
   }
 }
